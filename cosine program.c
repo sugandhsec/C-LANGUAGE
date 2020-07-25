952f1af9fb6183e685d83c6c6a55116000bf83bb
sugandhsec <sugandhsec112@gmail.com>
2020-07-25 23:36:48 +0530

    Cosine Program

---
+++ cosine program.c
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<stdio.h>
+#include <math.h>
+#define PI 3.1416
+#define MAX 180
+void main ( )
+{
+int angle;
+float x,y;
+angle = 0;
+printf("Angle Cos(angle)\n\n");
+while(angle <= MAX)
+{
+x = (PI/MAX)*angle;
+y = cos(x);
+printf("%15d %13.4f\n", angle, y);
+angle = angle + 10;
+}
+}
